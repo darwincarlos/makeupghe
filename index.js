f822f264f74c1a31e1877bcfb691c3ad5c5a8210
Darwin <furinax22@gmail.com>
2014-05-13 19:28:54 +0800

    index.js updated

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-var express = require("express");
+var express = require('express');
 var logfmt = require("logfmt");
-var app = express()
-  .use(express.bodyParser())
-  .use(express.static(__dirname + '/public'));
+var port = process.env.PORT || 3000;
+var app = express.createServer();
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
-
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.get('/', function(request, response) {
+    response.sendfile(__dirname + '/public/index.html');
+}).listen(port);
